bb550c0bc8cbc5ed936c3582d57d590b297372a8
jasonaboki-ops <jason.aboki@binghamuni.edu.ng>
2025-12-20 21:50:31 +0100

    Add files via upload

---
+++ Yorubadict.py
@@ -0,0 +1,23 @@
+yoruba_dict ={
+"Mother" : "iya",
+'How are you' : 'bawo ni',
+'house' : 'ile',
+'father' : 'baba',
+ 'good' : 'ounje',
+    "water" : 'omi',
+   'friend' : 'ore',
+   'eat' : 'je',
+   'farm' : 'oko',
+   'chair' : 'asuka',
+   'table' : 'tabili',
+   'fire' : 'ina',
+   'hat' : 'fila',
+   'phone' : 'mobili',
+   'car' : 'mota',
+   'bus' : 'baasi',
+   'board' : 'bawo',
+   'radio' : 'redio',
+   'pen' : 'pen',
+}
+word = input("choose a word: ")
+print(yoruba_dict[word])
