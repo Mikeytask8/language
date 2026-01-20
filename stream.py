6d9dc2abf961dc22a0ef8a294addfe65c5b11cb1
Micheal Chijindu <chimnoyerem.chijindu@binghamuni.edu.ng>
2026-01-20 23:08:07 +0100

    Create stream.py

---
+++ stream.py
@@ -0,0 +1,40 @@
+import streamlit as st 
+
+#1. define the dictionary data
+nigerian_dict= {
+    "igbo": {
+        "bekee": "english",
+        "akwa": "cloth",
+        "ahu": "body",
+        "nwoke": "boy",
+        "nwanyi": "girl"
+    },
+    "swahili": {
+        "jambo": "hello",
+        "kwa heri": "goodbye",
+        "asante": "thank you",
+        "maji": "water",
+        "chakula": "food" 
+    },
+    "hausa": {
+        "sannu": "hello",
+        "ina kwana": "good morning",
+        "na gode": "thank you",
+        "ruwa": "water",
+        "aboki": "friend"
+    },
+    "yoruba": {
+        "omi": "food",
+        "o d'aaro": "goodbye",
+        "jowo": "please",
+        "ore": "friend",
+        "e seun": "thank you"
+    },
+    "fulani": {
+        "jam": "hello",
+        "mi jaajoto": "sorry",
+        "e'e": "yes",
+        "aa'a": "no",
+        "ndiyam": "water"     
+    }
+}
